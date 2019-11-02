e46cc165a3f1030bf2f8e61287e06328e41315c9
Rajat K Wasan <rajatwasan12@gmail.com>
2019-11-03 02:48:33 +0530

    added plugins

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,9 @@ import { IServerConfigurations } from "./configurations";
 import * as crawler from "./api/crawler";
 import { IDatabase } from "./database";
 import * as http from 'http';
+import { IPlugin } from "./plugins/interfaces";
 
 http.globalAgent.maxSockets = 5;
-export const agent = new http.Agent({ keepAlive: true, maxSockets: 5 });
-
 
 export async function init(
   configs: IServerConfigurations,
@@ -28,7 +27,25 @@ export async function init(
     if (configs.routePrefix) {
       server.realm.modifiers.route.prefix = configs.routePrefix;
     }
+    const plugins: Array<string> = configs.plugins;
+    const pluginOptions = {
+      database: database,
+      serverConfigs: configs
+    };
 
+    let pluginPromises: Promise<any>[] = [];
+
+    plugins.forEach((pluginName: string) => {
+      var plugin: IPlugin = require("./plugins/" + pluginName).default();
+      console.log(
+        `Register Plugin ${plugin.info().name} v${plugin.info().version}`
+      );
+      pluginPromises.push(plugin.register(server, pluginOptions));
+    });
+
+    await Promise.all(pluginPromises);
+
+    console.log("All plugins registered successfully.");
     console.log("Register Routes");
     crawler.init(server, configs, database);
     console.log("Routes registered sucessfully.");
